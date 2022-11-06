2ff678e2c849720eac6fa9129d8e3504c5b1ca95
hgene0929 <hgene0929@gmail.com>
2022-11-06 10:37:45 +0900

    :tada: begin a project

---
+++ src/main/java/com/hufcusfocus/hufsland/HufslandApplication.java
@@ -0,0 +1,13 @@
+package com.hufcusfocus.hufsland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HufslandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HufslandApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hufcusfocus/hufsland/HufslandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hufcusfocus.hufsland;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HufslandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
